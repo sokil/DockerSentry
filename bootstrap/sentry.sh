b313d28f5efa6e157445f9cfb1ff45ed815c3c70
Dmytro Sokil <dmytro.sokil@gmail.com>
2017-02-17 01:34:02 +0200

    some tetss

--- bootstrap/sentry.sh
+++ bootstrap/sentry.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export SENTRY_SECRET_KEY=$(sentry config generate-secret-key)
+# export SENTRY_SECRET_KEY=$(sentry config generate-secret-key)
 
 echo "UPGRADE"
 sentry upgrade
 
 echo "START"
-sentry
+sentry run
 
 echo "CRON"
 sentry run cron
