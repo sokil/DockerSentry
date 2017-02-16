b1cb3f6df03d96dd83f4b81a97d9d48f87b67dba
Dmytro Sokil <dmytro.sokil@gmail.com>
2017-02-17 01:19:18 +0200

    init

---
+++ bootstrap/sentry.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export SENTRY_SECRET_KEY=$(sentry config generate-secret-key)
+
+echo "UPGRADE"
+sentry upgrade
+
+echo "START"
+sentry
+
+echo "CRON"
+sentry run cron
+
+echo "WORKER"
+sentry run worker
